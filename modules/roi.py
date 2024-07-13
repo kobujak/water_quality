6a50d5e766834bcc16ff429ce66be370239083d8
Konrad Bujak <konrad.bujak09@gmail.com>
2024-07-13 17:24:21 +0200

    geemap initialize

--- modules/roi.py
+++ modules/roi.py
@@ -1,6 +1,8 @@
 import ee
+import geemap
 
 def getRoi():
+    geemap.ee_initialize()
     feature = ee.FeatureCollection("projects/ee-konradbujak09/assets/ROI_CU_HMP", {})
     roi = feature.geometry()
     return roi
